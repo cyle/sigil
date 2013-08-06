e7a0d65555c6fb53c8768296b52d8d1225283f60
Cyle <cylegage@gmail.com>
2013-08-06 16:57:28 -0400

    - started on graph service and how to properly handle the bonus JSON data that will be attached to nodes and edges

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import "net/http"
+import "encoding/json" // documentation: http://golang.org/pkg/encoding/json/
 
 //
 // using gorest: https://code.google.com/p/gorest/wiki/GettingStarted?tm=6
@@ -10,31 +11,66 @@ import "code.google.com/p/gorest"
 
 func main() {
 	fmt.Println("Oh dear, a graph database...")
-	gorest.RegisterService(new(CyleService))
+	gorest.RegisterService(new(GraphService))
 	http.Handle("/", gorest.Handle())    
 	http.ListenAndServe(":8777", nil)
 }
 
-type Welp struct {
-	Haha, Wut int
+type Node struct {
+	Id int
+	Name string
+	ExtraJSONBytes []byte
+	ExtraJSON []interface{}
 }
 
-type CyleService struct{
+type Edge struct {
+	Id int
+	Name string
+	Source int
+	Target int
+	ExtraJSON []byte
+}
+
+type GraphService struct{
     // service level config
     gorest.RestService    `root:"/" consumes:"application/json" produces:"application/json"`
-    theDeets gorest.EndPoint `method:"GET" path:"/welp/{Id:int}" output:"Welp"`
-	rootHandler gorest.EndPoint `method:"GET" path:"/" output:"string"`
+	// define routes
+    rootHandler gorest.EndPoint `method:"GET" path:"/" output:"string"`
+	getNodeHandler gorest.EndPoint `method:"GET" path:"/node/{Id:int}" output:"Node"`
+	getEdgeHandler gorest.EndPoint `method:"GET" path:"/edge/{Id:int}" output:"Edge"`
 }
 
-func(serv CyleService) RootHandler() string {
+func(serv GraphService) RootHandler() string {
 	return "oh hello"
 }
 
-func(serv CyleService) TheDeets(Id int) (u Welp){
-	fmt.Println("Asking for: ", Id)
-	u.Haha = Id // as a reflection test
-	u.Wut = 300 // as a random test to see what happens
-	fmt.Println("Giving ", u)
+func(serv GraphService) GetNodeHandler(Id int) (n Node){
+	fmt.Printf("Asking for node ID: %d \n", Id)
+	
+	n.Id = Id
+	n.Name = "Some Node"
+	n.ExtraJSONBytes = []byte(`[{"Name": "Platypus", "Order": "Monotremata"}]`)
+	
+	var tmp interface {}
+	err := json.Unmarshal(n.ExtraJSONBytes, &tmp)
+	if err != nil {
+			fmt.Println("error:", err)
+		}
+	//fmt.Printf("JSON: %T %+v \n", tmp, tmp)
+	n.ExtraJSON = tmp.([]interface{})
+	n.ExtraJSONBytes = nil
+	
+	fmt.Printf("Giving %+v \n", n)
+	return
+    //serv.ResponseBuilder().SetResponseCode(404).Overide(true)  //Overide causes the entity returned by the method to be ignored. Other wise it would send back zeroed object
+    //return
+}
+
+func(serv GraphService) GetEdgeHandler(Id int) (e Edge){
+	fmt.Printf("Asking for edge ID: %d \n", Id)
+	e.Id = Id
+	e.Name = "Some Edge"
+	fmt.Printf("Giving %+v \n", e)
 	return
     //serv.ResponseBuilder().SetResponseCode(404).Overide(true)  //Overide causes the entity returned by the method to be ignored. Other wise it would send back zeroed object
     //return
